ae2ae811ef160cb8222298e5111b55e78fd533d2
fertg <fertg92@gmail.com>
2026-01-26 23:00:22 +0100

    cord firma

--- coords.py
+++ coords.py
@@ -4,8 +4,8 @@ PDF_TEMPLATES = {
     "NACIONAL": {
         "base": "pdf/nacional.pdf",
         "categoria_encuentro": (260, 510),  # Fuente: Categoría del Encuentro [cite: 8]
-        "equipoA": (150, 480),           # Fuente: Encuentro [cite: 10]
-        "equipoB": (370, 480),           # Fuente: Encuentro [cite: 10]
+        "equipoA": (150, 490),           # Fuente: Encuentro [cite: 10]
+        "equipoB": (370, 490),           # Fuente: Encuentro [cite: 10]
         "fecha": (200, 460),               # Fuente: Fecha del encuentro [cite: 11]
         "trayecto_de": (150, 423),          # Fuente: De [cite: 13]
         "trayecto_a": (370, 423),          # Fuente: a
@@ -14,7 +14,7 @@ PDF_TEMPLATES = {
         "matricula": (200, 350),           # Fuente: Matrícula [cite: 19]
         "fdo_nombre": (340, 170),           # Fuente: Fdo. [cite: 22]
         "dni": (370, 140),                 # Fuente: D.N.I. núm. [cite: 28]
-        "firma": (350, 75),               # Fuente: FIRMA [cite: 21]
+        "firma": (320, 210),               # Fuente: FIRMA [cite: 21]
         "lugar_fecha_dia": (360, 312),     # Fuente: a __ de __ [cite: 25, 26]
         "lugar_fecha_mes": (450, 312),
         "lugar_fecha_anio": (550, 312)
