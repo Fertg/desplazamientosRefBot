87a3b549cc6cfa75c307d8ff16eaa4f12e481202
fertg <fertg92@gmail.com>
2026-01-26 22:53:31 +0100

    coordenadas mod

--- coords.py
+++ coords.py
@@ -4,19 +4,19 @@ PDF_TEMPLATES = {
     "NACIONAL": {
         "base": "pdf/nacional.pdf",
         "categoria_encuentro": (260, 510),  # Fuente: Categoría del Encuentro [cite: 8]
-        "equipoA": (150, 470),           # Fuente: Encuentro [cite: 10]
-        "equipoB": (370, 470),           # Fuente: Encuentro [cite: 10]
-        "fecha": (200, 430),               # Fuente: Fecha del encuentro [cite: 11]
-        "trayecto_de": (150, 415),          # Fuente: De [cite: 13]
-        "trayecto_a": (370, 415),          # Fuente: a
+        "equipoA": (150, 480),           # Fuente: Encuentro [cite: 10]
+        "equipoB": (370, 480),           # Fuente: Encuentro [cite: 10]
+        "fecha": (200, 460),               # Fuente: Fecha del encuentro [cite: 11]
+        "trayecto_de": (150, 423),          # Fuente: De [cite: 13]
+        "trayecto_a": (370, 423),          # Fuente: a
         "kms": (200, 395),                 # Fuente: Total Kms. [cite: 16]
         "vehiculo": (420, 370),            # Fuente: Marca y Modelo [cite: 18]
         "matricula": (200, 350),           # Fuente: Matrícula [cite: 19]
-        "fdo_nombre": (340, 175),           # Fuente: Fdo. [cite: 22]
+        "fdo_nombre": (340, 170),           # Fuente: Fdo. [cite: 22]
         "dni": (370, 140),                 # Fuente: D.N.I. núm. [cite: 28]
-        "firma": (350, 200),               # Fuente: FIRMA [cite: 21]
-        "lugar_fecha_dia": (360, 310),     # Fuente: a __ de __ [cite: 25, 26]
-        "lugar_fecha_mes": (450, 310),
-        "lugar_fecha_anio": (550, 310)
+        "firma": (350, 75),               # Fuente: FIRMA [cite: 21]
+        "lugar_fecha_dia": (360, 312),     # Fuente: a __ de __ [cite: 25, 26]
+        "lugar_fecha_mes": (450, 312),
+        "lugar_fecha_anio": (550, 312)
     }
 }
