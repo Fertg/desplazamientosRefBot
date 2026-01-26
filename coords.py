4e2ce10bac7cf1ae89da37f85a68dd50d9a63d1f
fertg <fertg92@gmail.com>
2026-01-26 21:18:31 +0100

    Añadido sistema de acceso y categorías2

---
+++ coords.py
@@ -0,0 +1,21 @@
+# coords.py
+
+PDF_TEMPLATES = {
+    "NACIONAL": {
+        "base": "pdf/nacional.pdf",
+        "categoria_encuentro": (220, 638),  # Fuente: Categoría del Encuentro [cite: 8]
+        "encuentro": (145, 615),           # Fuente: Encuentro [cite: 10]
+        "fecha": (185, 601),               # Fuente: Fecha del encuentro [cite: 11]
+        "trayecto_de": (90, 574),          # Fuente: De [cite: 13]
+        "trayecto_a": (210, 545),          # Fuente: a
+        "kms": (140, 532),                 # Fuente: Total Kms. [cite: 16]
+        "vehiculo": (260, 503),            # Fuente: Marca y Modelo [cite: 18]
+        "matricula": (140, 490),           # Fuente: Matrícula [cite: 19]
+        "fdo_nombre": (90, 447),           # Fuente: Fdo. [cite: 22]
+        "dni": (330, 447),                 # Fuente: D.N.I. núm. [cite: 28]
+        "firma": (100, 465),               # Fuente: FIRMA [cite: 21]
+        "lugar_fecha_dia": (245, 473),     # Fuente: a __ de __ [cite: 25, 26]
+        "lugar_fecha_mes": (310, 473),
+        "lugar_fecha_anio": (375, 473)
+    }
+}
