4e73360bb7ddad5d8c323558626281ad564938aa
fertg <fertg92@gmail.com>
2026-01-26 20:37:01 +0100

    Codigo Inicial

---
+++ bot.py
@@ -0,0 +1,19 @@
+import os
+from dotenv import load_dotenv
+from telegram.ext import Application, CommandHandler
+
+load_dotenv()  # 👈 ESTO ES LO QUE FALTABA
+
+TOKEN = os.getenv("BOT_TOKEN")
+
+async def start(update, context):
+    await update.message.reply_text("Bot activo. Introduce el código de acceso.")
+
+def main():
+    print("TOKEN =", TOKEN)  # 👈 solo para comprobar
+    app = Application.builder().token(TOKEN).build()
+    app.add_handler(CommandHandler("start", start))
+    app.run_polling()
+
+if __name__ == "__main__":
+    main()
