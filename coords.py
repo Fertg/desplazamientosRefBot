759cbe21e6f6c3178b9348aed93681e5fdb6c2cc
fertg <fertg92@gmail.com>
2026-01-26 22:34:09 +0100

    coordenadas

--- coords.py
+++ coords.py
@@ -3,20 +3,20 @@
 PDF_TEMPLATES = {
     "NACIONAL": {
         "base": "pdf/nacional.pdf",
-        "categoria_encuentro": (260, 500),  # Fuente: Categoría del Encuentro [cite: 8]
-        "equipoA": (150, 615),           # Fuente: Encuentro [cite: 10]
-        "equipoB": (150, 615),           # Fuente: Encuentro [cite: 10]
-        "fecha": (185, 601),               # Fuente: Fecha del encuentro [cite: 11]
-        "trayecto_de": (90, 574),          # Fuente: De [cite: 13]
-        "trayecto_a": (210, 545),          # Fuente: a
-        "kms": (140, 532),                 # Fuente: Total Kms. [cite: 16]
-        "vehiculo": (260, 503),            # Fuente: Marca y Modelo [cite: 18]
-        "matricula": (140, 490),           # Fuente: Matrícula [cite: 19]
-        "fdo_nombre": (90, 447),           # Fuente: Fdo. [cite: 22]
-        "dni": (330, 447),                 # Fuente: D.N.I. núm. [cite: 28]
-        "firma": (100, 465),               # Fuente: FIRMA [cite: 21]
-        "lugar_fecha_dia": (245, 473),     # Fuente: a __ de __ [cite: 25, 26]
-        "lugar_fecha_mes": (310, 473),
-        "lugar_fecha_anio": (375, 473)
+        "categoria_encuentro": (260, 510),  # Fuente: Categoría del Encuentro [cite: 8]
+        "equipoA": (150, 520),           # Fuente: Encuentro [cite: 10]
+        "equipoB": (370, 520),           # Fuente: Encuentro [cite: 10]
+        "fecha": (200, 430),               # Fuente: Fecha del encuentro [cite: 11]
+        "trayecto_de": (150, 430),          # Fuente: De [cite: 13]
+        "trayecto_a": (150, 430),          # Fuente: a
+        "kms": (200, 390),                 # Fuente: Total Kms. [cite: 16]
+        "vehiculo": (420, 370),            # Fuente: Marca y Modelo [cite: 18]
+        "matricula": (200, 350),           # Fuente: Matrícula [cite: 19]
+        "fdo_nombre": (340, 160),           # Fuente: Fdo. [cite: 22]
+        "dni": (370, 140),                 # Fuente: D.N.I. núm. [cite: 28]
+        "firma": (350, 230),               # Fuente: FIRMA [cite: 21]
+        "lugar_fecha_dia": (360, 320),     # Fuente: a __ de __ [cite: 25, 26]
+        "lugar_fecha_mes": (450, 320),
+        "lugar_fecha_anio": (550, 320)
     }
 }
